7efd425c0c57e19ecbeb5f91b193adbf878539fd
AliDeV <ahmadi.ali2219@gmail.com>
2022-07-09 21:59:18 +0430

    Fix Layout but still has problems in address of javascript files

--- Personal Website/Startup.cs
+++ Personal Website/Startup.cs
@@ -77,7 +77,7 @@ namespace Personal_Website
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Home}/{id?}");
             });
             app.UseCookiePolicy();
         }
