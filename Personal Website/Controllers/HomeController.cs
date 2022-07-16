55f451f7b8016eae1df4b03a09ce3a38506488c6
ali.ahmadi <ahmadi.ali2219@gmail.com>
2022-07-16 15:38:02 +0430

    Try to work on Preload

--- Personal Website/Controllers/HomeController.cs
+++ Personal Website/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
@@ -221,5 +222,11 @@ namespace Personal_Website.Controllers
                     }
             }
         }
+        //[Route("get")]
+        //public bool get()
+        //{
+        //    Thread.Sleep(2000);
+        //    return true;
+        //}
     }
 }
